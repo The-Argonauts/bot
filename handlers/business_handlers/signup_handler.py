1f9c7afab8e1d57f6ca1784fc15dc73b0bb35a96
sina <sina.moradi2003@yahoo,.com>
2025-01-08 17:22:48 +0330

    Add BusinessHandlers

---
+++ handlers/business_handlers/signup_handler.py
@@ -0,0 +1,50 @@
+from telegram import Update
+from telegram.ext import ConversationHandler, CommandHandler, MessageHandler, filters, ContextTypes
+
+from services.BusinessService import BusinessService
+# States
+NAME, USERNAME, PASSWORD = range(3)
+
+
+class BusinessSignupHandler:
+    def __init__(self):
+        self.handler = ConversationHandler(
+            entry_points=[CommandHandler("user_signup", self.start)],
+            states={
+                NAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.name)],
+                USERNAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.username)],
+                PASSWORD: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.password)],
+            },
+            fallbacks=[CommandHandler("cancel", self.cancel)],
+        )
+        self.business_service = BusinessService()
+
+    async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+        await update.message.reply_text("Please enter your name.")
+        return NAME
+
+    async def name(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+        context.user_data["name"] = update.message.text
+        await update.message.reply_text("Please enter your username.")
+        return USERNAME
+
+    async def username(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+        context.user_data["username"] = update.message.text
+        await update.message.reply_text("Please enter your password.")
+        return PASSWORD
+
+    async def password(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+        context.user_data["password"] = update.message.text
+        await update.message.reply_text("Signup complete! Thank you.")
+
+        self.business_service.create_business(
+            context.user_data["name"],
+            context.user_data["username"],
+            context.user_data["password"],
+        )
+
+        return ConversationHandler.END
+
+    async def cancel(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+        await update.message.reply_text("Signup cancelled.")
+        return ConversationHandler.END
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from telegram.ext import ApplicationBuilder
+
+from handlers.business_handlers.signup_handler import BusinessSignupHandler
 from handlers.start_handler import StartHandler
 from handlers.user_handlers.signup_handler import UserSignupHandler
 from handlers.cancel_handler import CancelHandler
@@ -8,12 +10,14 @@ def main():
     app = ApplicationBuilder().token("7554909272:AAFj4k-SlOk3aNeDZzP5ucB4dqvonfNM0Gw").build()
 
     start_handler = StartHandler()
-    signup_handler = UserSignupHandler()
+    user_signup_handler = UserSignupHandler()
+    business_signup_handler = BusinessSignupHandler()
     cancel_handler = CancelHandler()
 
     # Register handlers
     app.add_handler(start_handler.handler)
-    app.add_handler(signup_handler.handler)
+    app.add_handler(user_signup_handler.handler)
+    app.add_handler(business_signup_handler.handler)
     app.add_handler(cancel_handler.handler)
 
     print("Bot is running...")
