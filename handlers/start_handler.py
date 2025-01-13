34b02057efa9cfccd75c567cf95d335f57d81056
heliasah <helya.sah@gmail.com>
2025-01-13 07:58:22 +0330

    Add landing page to welcome message

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -8,5 +8,5 @@ class StartHandler:
 
     async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         await update.message.reply_text(
-            "Welcome to the bot! \n Use /user_signup to start the registration process for the user section. \n Use /business_signup to start the registration process for the business section.\n Use /user_login to start the login process for the user section.\n Use /business_login to start the login process for the business section."
+            "Welcome to the bot! Checkout our Website \n https://theargonauts.vercel.app/ \n Use /user_signup to start the registration process for the user section. \n Use /business_signup to start the registration process for the business section.\n Use /user_login to start the login process for the user section.\n Use /business_login to start the login process for the business section."
         )
