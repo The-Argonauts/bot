bf08a2b44698cfed6a95f3b3155f0769ac7b30ce
aparsa <aparsa.khadem@gmail.com>
2025-01-15 05:58:46 +0330

    die

--- configs/database.py
+++ configs/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv('mysql+pymysql://root:admin@172.17.0.1:3306/argo')
+DATABASE_URL = 'mysql+pymysql://root:admin@172.17.0.1:3306/argo'
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
